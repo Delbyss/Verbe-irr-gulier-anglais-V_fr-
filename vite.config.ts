fc54809ae208fdb25f5096b5f70c7d302019feea
HugoD <transferhugod@gmail.com>
2024-10-01 18:30:03 +0200

    test 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Verbe-irregulier-anglais--VF/', // Remplace <REPO> par le nom de ton dépôt
+  base: '/Verbe-irregulier-anglais--VF/', 
 });
