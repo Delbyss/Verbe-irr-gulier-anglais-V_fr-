eaaaae6ab02c75e976c25384ac19f2f6354d2563
HugoD <transferhugod@gmail.com>
2024-10-27 15:46:14 +0100

    mise en place de react 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import MyComponent from './MyComponent';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
+function App() {
+  //const [count, setCount] = useState(0)
 
-export default function App() {
-    return (
-      <div>
-        <h1 class="text-6xl font-bold">Hello word !</h1>
-        <p class="pt-10">temporaire, test</p>
-      </div>
-    )
-  }
+  return (
+    <>
+      <main className="w-screen h-screen bg-blue-200 flex items-center justify-center flex-col">
+        <div>
+          <h1 className="text-6xl font-bold">Hello word !</h1>
+          <p className="pt-10">temporaire, test</p>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
